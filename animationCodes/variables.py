d79ecb28873b66ae3ebe7643699cb79748ee2713
baharKaviani <bhr.kaviani@gmail.com>
2021-03-31 19:01:17 +0430

    add: example code

--- animationCodes/variables.py
+++ animationCodes/variables.py
@@ -40,4 +40,92 @@ class CodeAnalyzer(Scene):
     """
     def construct(self):
         ### part 0: introduction
-        
+        # TODO: add x svg file as a character
+
+        ### part 1: example code
+        codeTitle = TextMobject("Example Program:")
+        codeTitle.to_edge(LEFT, buff=0.8)
+        codeTitle.shift([-0.5, 3.5, 0])
+        codeTitle.scale(0.8)
+
+        self.play(
+            Write(codeTitle)
+        )
+        self.wait(1.5)
+
+        line = Line([-6.3, 3.2, 0], [-6.3, -1.7, 0])
+
+        # draw the code
+        code = VGroup()
+
+        l1 = TextMobject("\\textrm{static}", "\\textrm{ int}", "\\textrm{ a}", "\\textrm{ = 20;}")
+        for i, color in zip(l1, [YELLOW_C, BLUE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l1)
+
+        l2 = TextMobject("\\textrm{void}", "\\textrm{ func}", "\\textrm{()}")
+        for i, color in zip(l2, [BLUE, PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l2)
+
+        b1 = TextMobject("\\textrm{\\{}")
+        b1.set_color(color, WHITE)
+        code.add(b1)
+
+        #TODO: "%d"
+        l3 = TextMobject("    \\textrm{printf}", "\\textrm{(}", "\\textrm{ d,}", "\\textrm{ a}", "\\textrm{ );}")
+        for i, color in zip(l3, [PURPLE_C, GREEN_E, BLUE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l3)
+
+        l4 = TextMobject("    \\textrm{a}", "\\textrm{ = 100;}")
+        for i, color in zip(l4, [RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l4)
+
+        b2 = TextMobject("\\textrm{\\}}")
+        b2.set_color(color, WHITE)
+        code.add(b2)
+
+        l5 = TextMobject("\\textrm{int}", "\\textrm{ main}", "\\textrm{()}")
+        for i, color in zip(l5, [BLUE, PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l5)
+
+        code.add(b1)
+
+        code.add(l3)
+
+        b3 = TextMobject("    \\textrm{\\{}")
+        b3.set_color(color, WHITE)
+        code.add(b3)
+
+        l6 = TextMobject("        \\textrm{static}", "\\textrm{ int}", "\\textrm{ a}", "\\textrm{ = 10;}")
+        for i, color in zip(l6, [YELLOW_C, BLUE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l6)
+
+        #TODO: "%d"
+        l7 = TextMobject("        \\textrm{printf}", "\\textrm{(}", "\\textrm{ \"d,}", "\\textrm{ \\}", "\\textrm{n\", }", "\\textrm{ a}", "\\textrm{ );}")
+        for i, color in zip(l7, [PURPLE_C, WHITE, GREEN_E, WHITE, WHITE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l7)
+
+        for i, l in enumerate(code):
+            l.to_edge(LEFT, buff=0.7)
+            l.shift([0.2 * (len(l[0].get_tex_string()) - len(l[0].get_tex_string().lstrip())), -0.55 * i + 0.65, 0])
+
+        code.scale(0.85)
+        code.shift([0, 2.6, 0])
+
+        l1_x = code[0].get_x() + 0.11  # keeping it as temp value
+        l1_y = code[0].get_y()  # keeping it as temp value
+
+        l2_x = code[1].get_x() + 0.11  # keeping it as temp value
+        l2_y = code[1].get_y()  # keeping it as temp value
+
+        self.play(FadeInFrom(line, DOWN))
+
+        for l in code:
+            self.play(FadeInFrom(l, LEFT), run_time=0.5)
+        self.wait(1)
