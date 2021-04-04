5e7a529c37a429584aef91daed5c0b21e4dab7a3
baharKaviani <bhr.kaviani@gmail.com>
2021-04-05 02:27:43 +0430

    add: Tips_Static

--- animationCodes/variables.py
+++ animationCodes/variables.py
@@ -6,6 +6,8 @@ class scene(Scene):
         Beginning.construct(self)
         CodeAnalyzer.construct(self)
         Tips_Local.construct(self)
+        Tips_Global.construct(self)
+        Tips_Static.construct(self)
 
 class Beginning(Scene):
     """
@@ -607,7 +609,7 @@ class Tips_Global(Scene):
         code = VGroup()
 
         l0 = TextMobject("\\# include <stdio.h>")
-        l0.set_color(GREY)
+        l0.set_color(WHITE)
         code.add(l0)
 
         blank = TextMobject("BLANK LINE")
@@ -663,3 +665,190 @@ class Tips_Global(Scene):
             self.play(FadeInFrom(l, LEFT), run_time=0.5)
         self.wait(1)
 
+class Tips_Static(Scene):
+    """
+    Show the important tips of static variables.
+    """
+    def construct(self):
+        ### part 0: explaining the 3 tips as bullet points
+        title = TextMobject("A Few Tips:")
+        title.to_edge(LEFT, buff=0.8)
+        title.shift([-0.5, 3.5, 0])
+        title.scale(0.8)
+        self.play(Write(title))
+        self.wait(1.5)
+
+        bp1 = TextMobject("static variable", " keyword", ":", " static")
+        bp2 = TextMobject("static variable", " scope", ":"," keeps its value throughout the program and in between")
+        bp22 = TextMobject("function calls")
+        bp3 = TextMobject("static variable", " default value", ":", " zero")
+        bp1[0].set_color(YELLOW_C)
+        bp1[3].set_color(BLUE)
+        bp2[0].set_color(YELLOW_C)
+        bp2[3].set_color(BLUE)
+        bp22.set_color(BLUE)
+        bp3[0].set_color(YELLOW_C)
+        bp3[3].set_color(BLUE)
+        bp1.scale(0.7)
+        bp2.scale(0.7)
+        bp22.scale(0.7)
+        bp3.scale(0.7)
+        bp1.to_edge(LEFT, buff=0.8)
+        bp2.to_edge(LEFT, buff=0.8)
+        bp22.to_edge(LEFT, buff=0.8)
+        bp3.to_edge(LEFT, buff=0.8)
+        bp1.shift([0, 2.5, 0])
+        bp2.shift([0, 2, 0])
+        bp22.shift([3.35, 1.5, 0])
+        bp3.shift([0, 1, 0])
+        self.play(Write(bp1))
+        self.wait(1.5)
+        self.play(Write(bp2), run_time=2.5)
+        self.play(Write(bp22))
+        self.wait(1.5)
+        self.play(Write(bp3))
+        self.wait(1.5)
+
+        local_tip_pack = VGroup(bp1, bp2, bp22, bp3)
+
+        rect = SurroundingRectangle(local_tip_pack, buff=0.2)
+        rect.set_color(YELLOW_C)
+        self.play(Write(rect))
+        self.wait(1)
+
+        rect_pack = VGroup(local_tip_pack, rect)
+
+        mini_title = TextMobject("static variable tips")
+        bp1_small = TextMobject(" keyword", ":", " static")
+        bp2_small = TextMobject(" scope", ":", " available throughout the program")
+        bp22_small = TextMobject("and in between function calls")
+        bp3_small = TextMobject(" default value", ":", " zero")
+        mini_title.set_color(YELLOW_C)
+        bp1_small[2].set_color(BLUE)
+        bp2_small[2].set_color(BLUE)
+        bp22_small.set_color(BLUE)
+        bp3_small[2].set_color(BLUE)
+        mini_title.scale(0.75)
+        bp1_small.scale(0.55)
+        bp2_small.scale(0.55)
+        bp22_small.scale(0.55)
+        bp3_small.scale(0.55)
+        mini_title.to_edge(LEFT, buff=8.8)
+        bp1_small.to_edge(LEFT, buff=9)
+        bp2_small.to_edge(LEFT, buff=9)
+        bp22_small.to_edge(LEFT, buff=9)
+        bp3_small.to_edge(LEFT, buff=9)
+        mini_title.shift([0, -1, 0])
+        bp1_small.shift([0, -1.5, 0])
+        bp2_small.shift([0, -1.9, 0])
+        bp22_small.shift([0, -2.3, 0])
+        bp3_small.shift([0, -2.7, 0])
+
+        local_tip_pack_small = VGroup(mini_title, bp1_small, bp2_small, bp22_small, bp3_small)
+        rect_small = SurroundingRectangle(local_tip_pack_small, buff=0.2)
+        rect_small.set_color(YELLOW_C)
+        rect_pack_small = VGroup(local_tip_pack_small, rect_small)
+
+        self.play(Transform(rect_pack, rect_pack_small))
+        self.wait(1)
+        self.play(FadeOut(title))
+        self.wait(2)
+
+        ### part 1: example code
+
+        codetitle = TextMobject("Example Program:")
+        codetitle.to_edge(LEFT, buff=0.8)
+        codetitle.shift([-0.5, 3.5, 0])
+        codetitle.scale(0.8)
+        self.play(Write(codetitle))
+        self.wait(1.5)
+
+        line = Line([-6.3, 3, 0], [-6.3, -3.3, 0])
+
+        # draw the code
+        code = VGroup()
+
+        l0 = TextMobject("\\# include <stdio.h>")
+        l0.set_color(WHITE)
+        code.add(l0)
+
+        blank = TextMobject("BLANK LINE")
+        blank.set_color(WHITE)
+        blank.set_opacity(0)
+        code.add(blank)
+
+        l1 = TextMobject("\\textrm{void}", "\\textrm{ inc}", "\\textrm{()}")
+        for i, color in zip(l1, [BLUE, PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l1)
+
+        b1 = TextMobject("\\textrm{\\{}")
+        b1.set_color(color, WHITE)
+        code.add(b1)
+
+        l2 = TextMobject("    \\textrm{static}", "\\textrm{ int}", "\\textrm{ i}", "\\textrm{ = 1;}")
+        for i, color in zip(l2, [YELLOW_C, BLUE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l2)
+
+        l3 = TextMobject("    \\textrm{printf}", "\\textrm{(}", "\\textrm{ \"\%d, \"}", "\\textrm{,}",
+                         "\\textrm{ i}", "\\textrm{);}")
+        for i, color in zip(l3, [PURPLE_C, WHITE, GREEN, WHITE, RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l3)
+
+        l4 = TextMobject("    \\textrm{i}", "++;")
+        for i, color in zip(l4, [RED_E, WHITE]):
+            i.set_color(color)
+        code.add(l4)
+
+        b2 = TextMobject("\\textrm{\\}}")
+        b2.set_color(color, WHITE)
+        code.add(b2)
+
+        l5 = TextMobject("\\textrm{ int}", "\\textrm{ main}", "\\textrm{()}")
+        for i, color in zip(l5, [BLUE, PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l5)
+
+        b3 = TextMobject("\\textrm{\\{}")
+        b3.set_color(color, WHITE)
+        code.add(b3)
+
+        l6 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
+        for i, color in zip(l6, [PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l6)
+
+        l7 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
+        for i, color in zip(l7, [PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l7)
+
+        l8 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
+        for i, color in zip(l8, [PURPLE_C, WHITE]):
+            i.set_color(color)
+        code.add(l8)
+
+        b4 = TextMobject("\\textrm{\\}}")
+        b4.set_color(color, WHITE)
+        code.add(b4)
+
+        for i, l in enumerate(code):
+            l.to_edge(LEFT, buff=0.7)
+            l.shift([0.2 * (len(l[0].get_tex_string()) - len(l[0].get_tex_string().lstrip())), -0.55 * i + 0.65, 0])
+
+        code.scale(0.8)
+        code.shift([0, 2.8, 0])
+
+        l1_x = code[0].get_x() + 0.11  # keeping it as temp value
+        l1_y = code[0].get_y()  # keeping it as temp value
+
+        l2_x = code[1].get_x() + 0.11  # keeping it as temp value
+        l2_y = code[1].get_y()  # keeping it as temp value
+
+        self.play(FadeInFrom(line, DOWN))
+
+        for l in code:
+            self.play(FadeInFrom(l, LEFT), run_time=0.5)
+        self.wait(1)
