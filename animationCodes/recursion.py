8c7f7257901b7c080b2d2328fb9e315a583cfcd4
unknown <smatintavakolia@gmail.com>
2021-04-21 17:20:39 +0430

    finally managed to understand the zooomed scene logic!

--- animationCodes/recursion.py
+++ animationCodes/recursion.py
@@ -294,7 +294,8 @@ class Factorial(MovingCameraScene):
             val_child.move_to([3, 2, 0])
             val_child.scale(0.6)
 
-            edge = Arrow([node.get_x(), node.get_y(), 0], [node_child.get_x(), node_child.get_y(), 0])  # TODO: arrow head is too big!
+            edge = Arrow([node.get_x(), node.get_y(), 0],
+                         [node_child.get_x(), node_child.get_y(), 0])  # TODO: arrow head is too big!
 
             edge.scale(0.4)
 
@@ -312,10 +313,20 @@ class Factorial(MovingCameraScene):
             self.wait()
 
 
-class Fibonacci(MovingCameraScene):
+class Fibonacci(ZoomedScene):
     """
     Fibonacci Function.
     """
+
+    CONFIG = {
+        "zoom_factor": 0.3,
+        "zoomed_display_height": 1,
+        "zoomed_display_width": 6,
+        "image_frame_stroke_width": 20,
+        "zoomed_camera_config": {
+            "default_frame_stroke_width": 3,
+        },
+    }
 
     def construct(self):
         ### part 0: introduction
@@ -425,43 +436,45 @@ class Fibonacci(MovingCameraScene):
             self.wait(0.7)
 
             node = Circle()  # TODO: is circle the best shape?
-            node.move_to([3, 4, 0])
-            node.scale(0.75)
+            node.move_to([0, 4, 0])
+            node.scale(0.25)
             node.set_color(ORANGE)
             node.set_opacity(0.3)
 
             val = TextMobject("fib(", str(n), ") = ?")
-            val.move_to([3, 4, 0])
-            val.scale(0.6)
+            val.move_to([0, 4, 0])
+            val.scale(0.2)
 
             self.play(ShowCreation(node), Write(val))
 
             node_child1 = Circle()  # TODO: is circle the best shape?
-            node_child1.move_to([1, 2, 0])
-            node_child1.scale(0.75)
+            node_child1.move_to([-1.5, 3.5, 0])
+            node_child1.scale(0.25)
             node_child1.set_color(ORANGE)
             node_child1.set_fill()
             node_child1.set_opacity(0.3)
 
             node_child2 = Circle()  # TODO: is circle the best shape?
-            node_child2.move_to([5, 2, 0])
-            node_child2.scale(0.75)
+            node_child2.move_to([1.5, 3.5, 0])
+            node_child2.scale(0.25)
             node_child2.set_color(ORANGE)
             node_child2.set_opacity(0.3)
 
-            val_child1 = TextMobject("fact(", str(n - 2), ") = ?")
-            val_child1.move_to([1, 2, 0])
-            val_child1.scale(0.6)
+            val_child1 = TextMobject("fibo(", str(n - 2), ") = ?")
+            val_child1.move_to([-1.5, 3.5, 0])
+            val_child1.scale(0.2)
 
-            val_child2 = TextMobject("fact(", str(n - 1), ") = ?")
-            val_child2.move_to([5, 2, 0])
-            val_child2.scale(0.6)
+            val_child2 = TextMobject("fibo(", str(n - 1), ") = ?")
+            val_child2.move_to([1.5, 3.5, 0])
+            val_child2.scale(0.2)
 
-            edge1 = Arrow([node.get_x(), node.get_y(), 0], [node_child1.get_x(), node_child1.get_y(), 0])  # TODO: arrow head is too big!
-            edge2 = Arrow([node.get_x(), node.get_y(), 0], [node_child2.get_x(), node_child2.get_y(), 0])  # TODO: arrow head is too big!
+            edge1 = Arrow([node.get_x(), node.get_y(), 0],
+                          [node_child1.get_x(), node_child1.get_y(), 0])  # TODO: arrow head is too big!
+            edge2 = Arrow([node.get_x(), node.get_y(), 0],
+                          [node_child2.get_x(), node_child2.get_y(), 0])  # TODO: arrow head is too big!
 
-            edge1.scale(0.6)
-            edge2.scale(0.6)
+            edge1.scale(0.2)
+            edge2.scale(0.2)
 
             self.play(
                 Write(node_child1),
@@ -479,195 +492,108 @@ class Fibonacci(MovingCameraScene):
 
             self.wait()
 
+            ######################################3
 
-class Tips_Static(Scene):
-    """
-    Show the important tips of static variables.
-    """
+            # # Set objects
+            # dot = Dot().shift(UL * 2)
+            #
+            # image = ImageMobject(np.uint8([[0, 100, 30, 200],
+            #                                [255, 0, 5, 33]]))
+            # image.set_height(7)
+            # frame_text = TextMobject("Frame", color=PURPLE).scale(1.4)
+            # zoomed_camera_text = TextMobject("Zommed camera", color=RED).scale(1.4)
+            #
+            # self.add(image, dot)
 
-    def construct(self):
-        ### part 0: introduction
-        # TODO: add x svg file as a character
+            # Set camera
+            zoomed_camera = self.zoomed_camera
+            zoomed_display = self.zoomed_display
+            frame = zoomed_camera.frame
+            # zoomed_display_frame = zoomed_display.display_frame
 
-        ### part 1: explaining the 3 tips as bullet points
-        title = TextMobject("A Few Tips:")
-        title.to_edge(LEFT, buff=0.8)
-        title.shift([-0.5, 3.5, 0])
-        title.scale(0.8)
-        self.play(Write(title))
-        self.wait(1.5)
+            cluster = VGroup(node, node_child1, node_child2)
 
-        bp1 = TextMobject("static variable", " keyword", ":", " static")
-        bp2 = TextMobject("static variable", " scope", ":", " keeps its value throughout the program and in between")
-        bp22 = TextMobject("function calls")
-        bp3 = TextMobject("static variable", " default value", ":", " zero")
-        bp1[0].set_color(YELLOW_C)
-        bp1[3].set_color(BLUE)
-        bp2[0].set_color(YELLOW_C)
-        bp2[3].set_color(BLUE)
-        bp22.set_color(BLUE)
-        bp3[0].set_color(YELLOW_C)
-        bp3[3].set_color(BLUE)
-        bp1.scale(0.7)
-        bp2.scale(0.7)
-        bp22.scale(0.7)
-        bp3.scale(0.7)
-        bp1.to_edge(LEFT, buff=0.8)
-        bp2.to_edge(LEFT, buff=0.8)
-        bp22.to_edge(LEFT, buff=0.8)
-        bp3.to_edge(LEFT, buff=0.8)
-        bp1.shift([0, 2.5, 0])
-        bp2.shift([0, 2, 0])
-        bp22.shift([3.35, 1.5, 0])
-        bp3.shift([0, 1, 0])
-        self.play(Write(bp1))
-        self.wait(1.5)
-        self.play(Write(bp2), run_time=2.5)
-        self.play(Write(bp22))
-        self.wait(1.5)
-        self.play(Write(bp3))
-        self.wait(1.5)
+            frame.move_to(cluster)
+            frame.set_color(YELLOW)
 
-        local_tip_pack = VGroup(bp1, bp2, bp22, bp3)
+            # zoomed_display_frame.set_color(BLACK)
+            zoomed_display.shift(2 * RIGHT + 0.5 * UP)
 
-        rect = SurroundingRectangle(local_tip_pack, buff=0.2)
-        rect.set_color(YELLOW_C)
-        self.play(Write(rect))
-        self.wait(1)
+            # background zoomed_display
+            zd_rect = BackgroundRectangle(
+                zoomed_display,
+                fill_opacity=0,
+                buff=MED_SMALL_BUFF,
+            )
 
-        rect_pack = VGroup(local_tip_pack, rect)
+            self.add_foreground_mobject(zd_rect)
 
-        mini_title = TextMobject("static variable tips")
-        bp1_small = TextMobject(" keyword", ":", " static")
-        bp2_small = TextMobject(" scope", ":", " available throughout the program")
-        bp22_small = TextMobject("and in between function calls")
-        bp3_small = TextMobject(" default value", ":", " zero")
-        mini_title.set_color(YELLOW_C)
-        bp1_small[2].set_color(BLUE)
-        bp2_small[2].set_color(BLUE)
-        bp22_small.set_color(BLUE)
-        bp3_small[2].set_color(BLUE)
-        mini_title.scale(0.75)
-        bp1_small.scale(0.55)
-        bp2_small.scale(0.55)
-        bp22_small.scale(0.55)
-        bp3_small.scale(0.55)
-        mini_title.to_edge(LEFT, buff=8.8)
-        bp1_small.to_edge(LEFT, buff=9)
-        bp2_small.to_edge(LEFT, buff=9)
-        bp22_small.to_edge(LEFT, buff=9)
-        bp3_small.to_edge(LEFT, buff=9)
-        mini_title.shift([0, -1, 0])
-        bp1_small.shift([0, -1.5, 0])
-        bp2_small.shift([0, -1.9, 0])
-        bp22_small.shift([0, -2.3, 0])
-        bp3_small.shift([0, -2.7, 0])
+            # animation of unfold camera
+            unfold_camera = UpdateFromFunc(
+                zd_rect,
+                lambda rect: rect.replace(zoomed_display)
+            )
 
-        local_tip_pack_small = VGroup(mini_title, bp1_small, bp2_small, bp22_small, bp3_small)
-        rect_small = SurroundingRectangle(local_tip_pack_small, buff=0.2)
-        rect_small.set_color(YELLOW_C)
-        rect_pack_small = VGroup(local_tip_pack_small, rect_small)
+            # frame_text.next_to(frame, DOWN)
 
-        self.play(Transform(rect_pack, rect_pack_small))
-        self.wait(1)
-        self.play(FadeOut(title))
-        self.wait(2)
+            # Scale in     x   y  z
+            # to put it short, the ratios must be exactly the same. Otherwise, the frame will scale like the zoomed display
+            frame_scale_factor = [8, 3, 0]
+            zd_scale_factor = [2, 0.75, 0]
 
-        ### part 2: example code
+            zoomed_display.scale(zd_scale_factor)
 
-        codetitle = TextMobject("Example Program:")
-        codetitle.to_edge(LEFT, buff=0.8)
-        codetitle.shift([-0.5, 3.5, 0])
-        codetitle.scale(0.8)
-        self.play(Write(codetitle))
-        self.wait(1.5)
+            self.play(
+                ShowCreation(frame),
+                frame.animate.scale(frame_scale_factor),
+                # zoomed_display.animate.scale(scale_factor),
+                # FadeIn(frame_text)
+            )
 
-        line = Line([-6.3, 3, 0], [-6.3, -3.3, 0])
+            # Activate zooming
+            self.activate_zooming()
 
-        # draw the code
-        code = VGroup()
+            self.play(
+                # You have to add this line
+                self.get_zoomed_display_pop_out_animation(),
+                unfold_camera
+            )
 
-        l0 = TextMobject("hashtag include <stdio.h>")  # TODO: what's wrong with #?
-        l0.set_color(GREY)
-        code.add(l0)
+            # zoomed_camera_text.next_to(zoomed_display_frame, DOWN)
+            # self.play(FadeIn(zoomed_camera_text))
 
-        blank = TextMobject("BLANK LINE")
-        blank.set_color(WHITE)
-        blank.set_opacity(0)
-        code.add(blank)
-
-        l1 = TextMobject("\\textrm{ int}", "\\textrm{ main}", "\\textrm{()}")
-        for i, color in zip(l1, [BLUE, PURPLE_C, WHITE]):
-            i.set_color(color)
-        code.add(l1)
-
-        b1 = TextMobject("\\textrm{\\{}")
-        b1.set_color(color, WHITE)
-        code.add(b1)
-
-        l2 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
-        for i, color in zip(l2, [PURPLE_C, WHITE]):
-            i.set_color(color)
-        code.add(l2)
-
-        l3 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
-        for i, color in zip(l3, [PURPLE_C, WHITE]):
-            i.set_color(color)
-        code.add(l3)
-
-        l4 = TextMobject("    \\textrm{ inc}", "\\textrm{();}")
-        for i, color in zip(l4, [PURPLE_C, WHITE]):
-            i.set_color(color)
-        code.add(l4)
-
-        b2 = TextMobject("\\textrm{\\}}")
-        b2.set_color(color, WHITE)
-        code.add(b2)
-
-        l5 = TextMobject("\\textrm{void}", "\\textrm{ inc}", "\\textrm{()}")
-        for i, color in zip(l5, [BLUE, PURPLE_C, WHITE]):
-            i.set_color(color)
-        code.add(l5)
-
-        b3 = TextMobject("\\textrm{\\{}")
-        b3.set_color(color, WHITE)
-        code.add(b3)
-
-        l6 = TextMobject("    \\textrm{static}", "\\textrm{ int}", "\\textrm{ i}", "\\textrm{ = 1;}")
-        for i, color in zip(l6, [YELLOW_C, BLUE, RED_E, WHITE]):
-            i.set_color(color)
-        code.add(l6)
-
-        l7 = TextMobject("    \\textrm{printf}", "\\textrm{(}", "\\textrm{ \"\%d, \"}", "\\textrm{,}",
-                         "\\textrm{ i}", "\\textrm{);}")
-        for i, color in zip(l7, [PURPLE_C, WHITE, GREEN, WHITE, RED_E, WHITE]):
-            i.set_color(color)
-        code.add(l7)
-
-        l8 = TextMobject("    \\textrm{i}", "++;")
-        for i, color in zip(l8, [RED_E, WHITE]):
-            i.set_color(color)
-        code.add(l8)
-
-        b4 = TextMobject("\\textrm{\\}}")
-        b4.set_color(color, WHITE)
-        code.add(b4)
-
-        for i, l in enumerate(code):
-            l.to_edge(LEFT, buff=0.7)
-            l.shift([0.2 * (len(l[0].get_tex_string()) - len(l[0].get_tex_string().lstrip())), -0.55 * i + 0.65, 0])
-
-        code.scale(0.8)
-        code.shift([0, 2.8, 0])
-
-        l1_x = code[0].get_x() + 0.11  # keeping it as temp value
-        l1_y = code[0].get_y()  # keeping it as temp value
-
-        l2_x = code[1].get_x() + 0.11  # keeping it as temp value
-        l2_y = code[1].get_y()  # keeping it as temp value
-
-        self.play(FadeInFrom(line, DOWN))
-
-        for l in code:
-            self.play(FadeInFrom(l, LEFT), run_time=0.5)
-        self.wait(1)
+            # # Scale in     x   y  z
+            # scale_factor = [0.5, 1.5, 0]
+            #
+            # # Resize the frame and zoomed camera
+            # self.play(
+            #     frame.animate.scale(scale_factor),
+            #     zoomed_display.animate.scale(scale_factor),
+            #     # FadeOut(zoomed_camera_text),
+            #     # FadeOut(frame_text)
+            # )
+            #
+            # # Resize the frame
+            # self.play(
+            #     frame.animate.scale(3),
+            #     frame.animate.shift(2.5 * DOWN)
+            # )
+            #
+            # # Resize zoomed camera
+            # self.play(
+            #     ScaleInPlace(zoomed_display, 2)
+            # )
+            #
+            # self.wait()
+            #
+            # self.play(
+            #     self.get_zoomed_display_pop_out_animation(),
+            #     unfold_camera,
+            #     # -------> Inverse
+            #     rate_func=lambda t: smooth(1 - t),
+            # )
+            # self.play(
+            #     # Uncreate(zoomed_display_frame),
+            #     Uncreate(frame),
+            # )
+            # self.wait()
